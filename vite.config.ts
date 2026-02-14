8ba1d51482cf4d34de731cdc8354f86d7ea65dc8
reyatechprojects <reyatechprojects@gmail.com>
2026-02-14 19:52:30 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/tvs-campus-navigator/", // ✅ MUST match repo name
+base: "/tvs-campus-navigator/",
 
   plugins: [react()],
 
